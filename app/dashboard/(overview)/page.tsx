4d0d6edcce503e40510db9f0f052c949ed91a555
prathamesh0987 <psvagare@gmail.com>
2024-01-08 16:22:43 +0530

    add streaming to latest invoices of dashboard

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,13 +2,13 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
+import { fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
-import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
  
 export default async function Page() {
 
-    const latestInvoices=await fetchLatestInvoices();
+    
     const{
         numberOfCustomers,
       numberOfInvoices,
@@ -34,8 +34,10 @@ export default async function Page() {
         <Suspense fallback={<RevenueChartSkeleton/>}>
         <RevenueChart/>
         </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton/>}>
+        <LatestInvoices/>
+        </Suspense>
         
-        <LatestInvoices latestInvoices={latestInvoices}/>
         
       </div>
     </main>
